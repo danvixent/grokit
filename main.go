9bf9be82ac17bd7b8b72848c8508b9af11031eec
Daniel Oluojomu <danvixent@gmail.com>
2021-11-22 10:42:24 +0100

    add code

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	mux := http.DefaultServeMux
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("received connection from", r.RemoteAddr)
+		w.WriteHeader(http.StatusOK)
+	})
+
+	fmt.Println("starting http listener")
+
+	srv := http.Server{Addr: ":" + port, Handler: mux}
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			logrus.WithError(err).Fatalln("failed to listen")
+		}
+	}()
+
+	//Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		logrus.WithError(err).Fatalln("graceful shutdown failed")
+	}
+
+	logrus.Infoln("closed listener")
+}

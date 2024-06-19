ce0220aae775e505164356c4d2be7d0bf9da4806
Daniel Oluojomu <danvixent@gmail.com>
2024-06-19 11:02:03 +0100

    remove body print

--- main.go
+++ main.go
@@ -23,6 +23,22 @@ func main() {
 
 	mux := http.DefaultServeMux
 
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("received connection to / from", r.RemoteAddr)
+		w.WriteHeader(http.StatusOK)
+		r.Body.Close()
+
+		//b, err := io.ReadAll(r.Body)
+		//if err != nil {
+		//	logrus.Print("body read err", err)
+		//}
+		//fmt.Println("body:", string(b))
+		sema <- struct{}{}
+		count++
+		fmt.Println("count", count)
+		<-sema
+	})
+
 	mux.HandleFunc("/endpoint1", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("received connection to endpoint1 from", r.RemoteAddr)
 		w.WriteHeader(http.StatusOK)
